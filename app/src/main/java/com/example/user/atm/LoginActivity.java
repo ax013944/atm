fdbfe415a3ab950a57ab0534abb2452d74346b60
ax013944 <ax013944@gmail.com>
2018-10-30 15:42:47 +0800

    atm2

--- app/src/main/java/com/example/user/atm/LoginActivity.java
+++ app/src/main/java/com/example/user/atm/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.user.atm;
 
-
-
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,37 +8,31 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.EditText;
-
-
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
-
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
-
     }
-
-
-
     public void login(View view) {
-
         String userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
-
         String passwd = ((EditText)findViewById(R.id.ed_password)).getText().toString();
-
         if ("jack".equals(userid) && "1234".equals(passwd)) {
-
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            getIntent().putExtra("Login_USERID",userid);
+            getIntent().putExtra("Login_PASSWORD",passwd);
+            setResult(RESULT_OK,getIntent());
             finish();
+        }else {
+            new AlertDialog.Builder(this)
+                    .setTitle("ATM")
+                    .setMessage("登入失敗")
+            .setPositiveButton("OK",null)
+                    .show();
 
         }
-
     }
-
+    public void cancel(View v){}
 }
--- app/src/main/java/com/example/user/atm/MainActivity.java
+++ app/src/main/java/com/example/user/atm/MainActivity.java
@@ -4,22 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_LOGIN = 1;
     boolean logon = false;
+    @Override
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==RC_LOGIN){
+            if(resultCode == RESULT_OK){
+                String uid=data.getStringExtra("Login_USERID");
+                String pw=data.getStringExtra("Login_PASSWORD");
+            }
+        }
+    }
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         if (!logon) {
-
             Intent intent = new Intent(this, LoginActivity.class);
-
             startActivity(intent);
-
+            startActivityForResult(intent,RC_LOGIN);
         }
     }
 }
